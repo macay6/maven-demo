53bce4e21a0a4aca8e1adc0cd8845e2778d857a2
macay <1207260521@qq.com>
2023-05-01 14:10:04 +0800

    kafka-demo

--- kafka-demo/src/main/java/producer/AsyncCallbackProducer.java
+++ kafka-demo/src/main/java/producer/AsyncCallbackProducer.java
@@ -18,7 +18,7 @@ public class AsyncCallbackProducer {
 
         // 2. 给 kafka 配置对象添加配置信息：bootstrap.servers
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "172.16.8.46:9092, 172.16.8.46:9093, 172.16.8.46:9094");
+                "192.168.3.9:9092, 192.168.3.9:9093, 192.168.3.9:9094");
 
         // 3、key,value 序列化（必须）：key.serializer，value.serializer
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
@@ -37,6 +37,7 @@ public class AsyncCallbackProducer {
             for (int i = 0; i <5 ; i++) {
                 // 依次指定 key 值为 a,b,f ，数据 key 的 hash 值与 2 个分区求余，分别发往 1、0
                 producer.send(new ProducerRecord<String, String>("TestTopic", "hello", "macay999" + i), new Callback() {
+                    @Override
                     public void onCompletion(RecordMetadata recordMetadata, Exception exception) {
                         if (exception == null) {
                             System.out.println("主题：" + recordMetadata.topic() + ",分区：" + recordMetadata.partition() );
